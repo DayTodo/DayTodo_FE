9c866dc8ff3fab8365dc5024bdea6ac061f74194
hw4nx02 <2002ckdghks@naver.com>
2026-07-21 02:33:09 +0900

    fix: 홈에서 사용하는 relationship에 따른 텍스트 포맷 파일 수정

--- feature/home/src/main/kotlin/com/team_daytodo/daytodo/feature/home/presentation/HomeTextFormatters.kt
+++ feature/home/src/main/kotlin/com/team_daytodo/daytodo/feature/home/presentation/HomeTextFormatters.kt
@@ -3,15 +3,4 @@ package com.team_daytodo.daytodo.feature.home.presentation
 import com.team_daytodo.daytodo.core.model.Relationship
 
 internal val Relationship.courseCompanionLabel: String
-    get() = when (this) {
-        Relationship.FRIEND -> "친구들"
-        Relationship.LOVER -> "연인"
-        Relationship.FAMILY -> "가족"
-    }
-
-internal val Relationship.scheduleTargetLabel: String
-    get() = when (this) {
-        Relationship.FRIEND -> "친구와"
-        Relationship.LOVER -> "연인과"
-        Relationship.FAMILY -> "가족과"
-    }
+    get() = companionLabel
