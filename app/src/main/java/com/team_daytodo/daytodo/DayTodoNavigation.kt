8c39bf64a7d6dfa3b59b9d7eac008f8aac32882e
hw4nx02 <2002ckdghks@naver.com>
2026-07-25 17:39:43 +0900

    feat: 비밀번호 찾기 화면 UI 구현

--- app/src/main/java/com/team_daytodo/daytodo/DayTodoNavigation.kt
+++ app/src/main/java/com/team_daytodo/daytodo/DayTodoNavigation.kt
@@ -8,6 +8,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
+import com.team_daytodo.daytodo.feature.auth.FindPasswordRoute
 import com.team_daytodo.daytodo.feature.auth.LoginRoute
 import com.team_daytodo.daytodo.feature.auth.ProfileSetupRoute
 import com.team_daytodo.daytodo.feature.auth.ResetPasswordRoute
