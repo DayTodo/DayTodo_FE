2babcae9372205411ce7d897d821ae0605c073b9
hw4nx02 <2002ckdghks@naver.com>
2026-07-26 18:20:23 +0900

    feat: 홈-저장 화면 연결 및 더미데이터 원복

--- feature/home/src/main/kotlin/com/team_daytodo/daytodo/feature/home/model/HomeMagazineUiModel.kt
+++ feature/home/src/main/kotlin/com/team_daytodo/daytodo/feature/home/model/HomeMagazineUiModel.kt
@@ -1,6 +1,7 @@
 package com.team_daytodo.daytodo.feature.home.model
 
 data class HomeMagazineUiModel(
+    val placeId: String,
     val title: String,
     val location: String,
     val description: String,
