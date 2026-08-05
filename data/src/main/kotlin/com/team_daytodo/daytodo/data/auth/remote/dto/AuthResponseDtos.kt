12adb579363668292bbae7141ee91acb3645cdc5
hw4nx02 <2002ckdghks@naver.com>
2026-08-05 14:10:37 +0900

    feat: :auth API request, response dto 작성

---
+++ data/src/main/kotlin/com/team_daytodo/daytodo/data/auth/remote/dto/AuthResponseDtos.kt
@@ -0,0 +1,68 @@
+package com.team_daytodo.daytodo.data.auth.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class LoginResponseDto(
+    @SerialName("accessToken")
+    val accessToken: String,
+    @SerialName("refreshToken")
+    val refreshToken: String,
+    @SerialName("username")
+    val username: UserDto,
+)
+
+@Serializable
+data class UserDto(
+    @SerialName("userId")
+    val userId: Int,
+    @SerialName("nickname")
+    val nickname: String,
+)
+
+@Serializable
+data class TokenRefreshResponseDto(
+    @SerialName("accessToken")
+    val accessToken: String,
+    @SerialName("refreshToken")
+    val refreshToken: String,
+)
+
+@Serializable
+data class LinkNaverResponseDto(
+    @SerialName("socialAccountId")
+    val socialAccountId: Int,
+    @SerialName("provider")
+    val provider: String,
+    @SerialName("linkedAt")
+    val linkedAt: String,
+)
+
+@Serializable
+data class EmailCheckResponseDto(
+    @SerialName("email")
+    val email: String,
+    @SerialName("available")
+    val available: Boolean,
+)
+
+@Serializable
+data class RegisterResponseDto(
+    @SerialName("userId")
+    val userId: Int,
+    @SerialName("email")
+    val email: String,
+    @SerialName("nickname")
+    val nickname: String,
+    @SerialName("createdAt")
+    val createdAt: String,
+)
+
+@Serializable
+data class VerifyEmailResponseDto(
+    @SerialName("email")
+    val email: String,
+    @SerialName("verified")
+    val verified: Boolean,
+)
