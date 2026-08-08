baeb6e3ec36cb08170f4416f522545985a6d5736
hajinmn-stack <hajinmn@ewha.ac.kr>
2026-08-08 11:58:00 +0900

    fix: 마이페이지 비밀번호 변경 화면에서 바텀 네비게이션 숨김 처리 누락 수정

--- app/src/main/java/com/team_daytodo/daytodo/DayTodoApp.kt
+++ app/src/main/java/com/team_daytodo/daytodo/DayTodoApp.kt
@@ -38,6 +38,7 @@ fun DayTodoApp() {
         DayTodoRoute.Calendar,
         MypageRoute.ProfileEdit,
         MypageRoute.PhoneChange,
+        MypageRoute.PasswordChange,
         RecordRoute.PhotoSelect,
         RecordRoute.Memo,
     )
